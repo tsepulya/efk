14a08324ef391706ca07146ab5b8bbf0e29d0d7f
tsepulya <tsepeleva_anna@mail.ru>
2021-12-09 22:25:08 +0300

    feat: add active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Menu from '../UI/UiMenu/UiMenu';
 import Switcher from '../UI/UiSwitcher/UiSwitcher';
 
 import styles from './Header.module.css';
 
 const Header = () => (
   <header className={styles.header}>
+    <Menu />
     <Switcher />
   </header>
 );
