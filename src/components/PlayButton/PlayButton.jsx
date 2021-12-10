81d2458170b508b1654a31b2d1227d9d6260968c
tsepulya <tsepeleva_anna@mail.ru>
2021-12-10 21:52:33 +0300

    feat: add repeat btn style

--- src/components/PlayButton/PlayButton.jsx
+++ src/components/PlayButton/PlayButton.jsx
@@ -1,16 +1,30 @@
-/* eslint-disable react/jsx-indent */
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './PlayButton.module.css';
+import repeat from '../../assets/common/img/repeat.svg';
+
+const START = 'Start game';
+
+const RepeatBtn = () => (
+  <img src={repeat} alt="repeat" className={styles.btn__repeat_img} />
+);
 
 const PlayButton = () => {
-  return (
-      <div>
-    <button type="button" className={styles.btn__play_start}>
-      Start game
-    </button>
-      </div>
+  const [innerBtn, setInnerBtn] = useState(START);
 
+  const changeBtn = () => {
+    if (innerBtn === START) {
+      setInnerBtn(<RepeatBtn />);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={innerBtn === START ? styles.btn__play_start : styles.btn__play_reset}
+      onClick={() => changeBtn()}
+    >
+      {innerBtn}
+    </button>
   );
 };
 
